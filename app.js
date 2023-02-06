afa188d34551bb18c17367c80c30940a3f74bbc2
Isab2005 <123569430+Isab2005@users.noreply.github.com>
2023-02-06 12:04:46 -0500

    vars

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "descarga1",
+      img: "Imagenes/descarga1.jpg"
+    },
+    {
+      name: "descarga2",
+      img: "Imagenes/descarga2.jpg"
+    },
+    {
+      name: "descarga3",
+      img: "Imagenes/descarga3.jpg"
+    },
+    {
+      name: "descarga4",
+      img: "Imagenes/descarga4.jpg"
+    },
+    {
+      name: "descarga5",
+      img: "Imagenes/descarga5.jpg"
+    },
+    {
+      name: "descarga6",
+      img: "Imagenes/descarga6.jpg"
+    }
+  ];
+});
